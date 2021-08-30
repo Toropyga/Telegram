327d4cbcf16d7faa6e7a3d5e852facf5f0c53d8c
Yuri Frantsevich <frantsevich@gmail.com>
2021-08-30 08:47:38 +0300

    Added support for Webhook Changed function sendToTelegram Added functions "setToken" and "setChatID"

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Telegram
 Отправка сообщений от имени Телеграм бота
 
+![License](https://img.shields.io/badge/license-MIT-brightgreen.svg)
+![Version](https://img.shields.io/badge/version-v2.2.0-blue.svg)
+![PHP](https://img.shields.io/badge/php-v5.5_--_v8-blueviolet.svg)
+
 # Содержание
 
 - [Общие понятия](#общие-понятия)
@@ -9,8 +13,13 @@
 - [Описание работы](#описание-работы)
     - [Подключение файла класса](#Подключение-файла-класса)
     - [Инициализация класса](#Инициализация-класса)
-    - [Установка метода взаимодействия с API Телеграм](#Установка-метода-взаимодействия-с-API-Телеграм)
+    - [Установка метода взаимодействия с API Телеграм](#Установка-метода-взаимодействия-с-API-Телеграм-при-отправке-сообщений)
         - [Поддерживаемые методы](#Поддерживаемые-методы)
+    - [Установка токена телеграм бота](#Установка-токена-телеграм-бота)
+    - [Установка параметров Webhook-сервера](#Установка-параметров-сервера-Webhook)
+    - [Удаление сервера Webhook](#Удаление-сервера-Webhook)
+    - [Получение информации о сервере Webhook](#Получение-информации-о-сервере-Webhook)
+    - [Установка идентификатора получателя](#Установка-идентификатора-получателя)
     - [Включение отладки](#Включение-отладки)
     - [Получение логов](#Получение-логов)
     - [Пример отправки сообщения](#Пример-отправки-сообщения)
@@ -64,7 +73,7 @@ use \FYN\Base;
 $telegram = new FYN\Telegram();
 ```
 ---
-## Установка метода взаимодействия с API Телеграм
+## Установка метода взаимодействия с API Телеграм при отправке сообщений
 ```php
 $telegram->setMethod('sendVideo');
 ```
@@ -76,6 +85,41 @@ $telegram->setMethod('sendVideo');
 * ['sendAudio'](https://core.telegram.org/bots/api#sendaudio) - отправка звукового файла,
 * ['sendVoice'](https://core.telegram.org/bots/api#sendvoice) - отправка голосового сообщения,
 * ['sendAnimation'](https://core.telegram.org/bots/api#sendanimation) - отправка анимированного изображения.
+---
+## Установка токена телеграм бота
+```php
+$telegram->setToken("000000000:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+```
+
+---
+## Установка параметров сервера Webhook
+```php
+$webhook_url = 'https://your_server_path'; // адрес Webhook-сервера
+$certificate = '/home/user/ssl/my_ssl'; // путь к персональному сертификату (необязательный параметр)
+$ip_address = '000.000.000.000'; // IP-адрес , который будет использоваться для отправки запросов Webhook вместо IP-адреса, полученного через DNS (необязательный параметр)
+$max_connections = 40; // максимальное количество одновременных подключений к Webhook-серверу [1-100] (необязательный параметр)
+
+$telegram->setWebhook ($webhook_url, $certificate, $ip_address, $max_connections);
+```
+
+---
+## Удаление сервера Webhook
+```php
+$telegram->deleteWebhook();
+```
+
+---
+## Получение информации о сервере Webhook
+```php
+$info = $telegram->getWebhookInfo();
+```
+
+---
+## Установка идентификатора получателя
+```php
+$telegram->setChatID("000000000");
+```
+
 ---
 ## Включение отладки
 ```php
@@ -96,12 +140,16 @@ $telegram->getLogs();
 $token = "000000000:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // токен Телеграм бота
 $chat_id = 000000000; // ID чата получателя
 $telegram->setMethod('sendMessage');
-if (!$answer = $telegram->sendToTelegram($message, $token, $chat_id)) Base::dump($telegram->getLogs());
+
+$telegram->setToken($token);
+$telegram->setChatID($chat_id);
+if (!$answer = $telegram->sendToTelegram($message)) Base::dump($telegram->getLogs());
 else Base::dump($answer);
 ```
 или отправка сообщения с использованием параметров по умолчанию
 ```php
 $message = "Hi!";
+
 if (!$answer = $telegram->sendToTelegram($message)) Base::dump($telegram->getLogs());
 else Base::dump($answer);
 ```
@@ -110,17 +158,32 @@ else Base::dump($answer);
 ```php
 $token = "000000000:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // токен Телеграм бота
 $chat_id = 000000000; // ID чата получателя
+$reply_message_id = 0000; // ID сообщения на которое даём ответ, по умолчанию - 0;
 $caption = "It's my photo!";
 $path_to_photo = "/home/images/my.jpeg";
+
 $telegram->setDebug(true);
+$telegram->setToken($token);
+$telegram->setChatID($chat_id);
 $telegram->setMethod('sendPhoto');
-if (!$answer = $telegram->sendToTelegram($caption, $path_to_photo, $token, $chat_id)) Base::dump($telegram->getLogs());
+if (!$answer = $telegram->sendToTelegram($caption, $path_to_photo, $reply_message_id)) Base::dump($telegram->getLogs());
+else Base::dump($answer);
+```
+отправка документа пользователю
+```php
+$chat_id = 111111111; // ID чата получателя
+$caption = "It's my photo!";
+$path_to_photo = "/home/images/my.jpeg";
+
+$telegram->setMethod('sendPhoto');
+if (!$answer = $telegram->sendToTelegram($caption, $path_to_photo, 0, $chat_id)) Base::dump($telegram->getLogs());
 else Base::dump($answer);
 ```
 или отправка фотографии с использованием параметров по умолчанию
 ```php
 $caption = "It's my photo!";
 $path_to_photo = "/home/images/my.jpeg";
+
 $telegram->setMethod('sendPhoto');
 if (!$answer = $telegram->sendToTelegram($caption, $path_to_photo)) Base::dump($telegram->getLogs());
 else Base::dump($answer);
--- src/Telegram.php
+++ src/Telegram.php
@@ -5,7 +5,7 @@
  * @author Yuri Frantsevich (FYN)
  * Date: 20/08/2011
  * Time: 08:46
- * @version 2.1.1
+ * @version 2.2.0
  * @copyright 2021
  */
 
@@ -23,7 +23,7 @@ class Telegram {
     private $telegram_token = '';
 
     /**
-     * ID чата в телеграмм
+     * ID чата в телеграмм (кому отправляем сообщение)
      * ИД чата telegram получаем через запрос https://api.telegram.org/bot{token}/getUpdates
      * @var integer
      */
@@ -98,30 +98,124 @@ class Telegram {
      * @param string $method
      */
     public function setMethod ($method = 'sendMessage') {
-        $avaliable_methods = array('sendMessage', 'sendPhoto', 'sendDocument', 'sendVideo', 'sendAudio', 'sendVoice', 'sendAnimation');
-        if (in_array($method, $avaliable_methods)) {
+        $available_methods = array('sendMessage', 'sendPhoto', 'sendDocument', 'sendVideo', 'sendAudio', 'sendVoice', 'sendAnimation');
+        if (in_array($method, $available_methods)) {
             if ($this->debug) $this->logs[] = "Telegram method set to ".$method;
             $this->method = $method;
         }
     }
 
     /**
+     * Установка токена
+     * @param string $token - токен от телеграмм бота
+     */
+    public function setToken ($token) {
+        $this->telegram_token = $token;
+        if ($this->debug) $this->logs[] = "Telegram set token: ".$token;
+    }
+
+    /**
+     * Установка ID чата в телеграм (кому отправляем сообщение)
+     * @param integer $chatId - ID чата в телеграм
+     */
+    public function setChatID ($chat_id) {
+        $this->telegram_id = $chat_id;
+        if ($this->debug) $this->logs[] = "Telegram set Chat ID: ".$chat_id;
+    }
+
+    /**
+     * Установка параметров сервера обработки запросов (Webhook-сервер)
+     * @param string $webhook_url - адрес Webhook-сервера
+     * @param string $certificate - путь к персональному сертификату (необязательный параметр)
+     * @param string $ip_address - IP-адрес , который будет использоваться для отправки запросов Webhook вместо IP-адреса, полученного через DNS (необязательный параметр)
+     * @param int $max_connections - максимальное количество одновременных подключений к Webhook-серверу [1-100] (необязательный параметр)
+     * @return bool|mixed
+     */
+    public function setWebhook ($webhook_url, $certificate = '', $ip_address = '', $max_connections = 0) {
+        if (!$webhook_url) {
+            $this->logs[] = "Wrong Telegram parameters!";
+            $this->logs[] = "Webhook url not defined!";
+            return false;
+        }
+        $url = "https://api.telegram.org/bot" . $this->telegram_token . "/setWebhook";
+        if ($this->debug) $this->logs[] = "Telegram URL: ".$url;
+        $file_curl = [];
+        if ($certificate) {
+            if (file_exists($certificate)) {
+                if ($this->debug) $this->logs[] = "Telegram certificate: " . $certificate;
+                $file_curl = new \CURLFile(realpath($certificate));
+            }
+            else {
+                $this->logs[] = "Certificate not found";
+                return false;
+            }
+        }
+        $data = array();
+        $data['url'] = $webhook_url;
+        if ($certificate && $file_curl) $data['certificate'] = $file_curl;
+        if ($ip_address) $data['ip_address'] = $ip_address;
+        if ($max_connections && (int) $max_connections > 0 && (int) $max_connections <= 100) $data['max_connections'] = (int) $max_connections;
+
+        $t_bot = json_decode($this->net->getContent($url, 5, $data), true);
+        if (!isset($t_bot['ok'])) {
+            $t_bot = false;
+            $this->logs[] = "Telegram SEND ERROR!";
+        }
+        elseif (isset($t_bot['error_code']) && $t_bot['error_code']) {
+            $this->logs[] = $t_bot['error_code'].": ".$t_bot['description'];
+            $t_bot = false;
+        }
+        if ($this->debug && is_array($t_bot)) $this->logs[] = "Telegram send status: ".$t_bot['ok'];
+        elseif ($this->debug) $this->logs[] = "Telegram send status: ".$t_bot;
+        return $t_bot;
+    }
+
+    /**
+     * Удаление Webhook-сервера для бота
+     * @return bool|mixed
+     */
+    public function deleteWebhook () {
+        $url = "https://api.telegram.org/bot" . $this->telegram_token . "/deleteWebhook";
+        if ($this->debug) $this->logs[] = "Telegram URL: ".$url;
+        $t_bot = json_decode($this->net->getContent($url, 5), true);
+        if (!isset($t_bot['ok'])) {
+            $t_bot = false;
+            $this->logs[] = "Telegram SEND ERROR!";
+        }
+        elseif (isset($t_bot['error_code']) && $t_bot['error_code']) {
+            $this->logs[] = $t_bot['error_code'].": ".$t_bot['description'];
+            $t_bot = false;
+        }
+        if ($this->debug && is_array($t_bot)) $this->logs[] = "Telegram send status: ".$t_bot['ok'];
+        elseif ($this->debug) $this->logs[] = "Telegram send status: ".$t_bot;
+        return $t_bot;
+    }
+
+    /**
+     * Получение информации о Webhook-сервере бота
+     * @return mixed
+     */
+    public function getWebhookInfo () {
+        $url = "https://api.telegram.org/bot" . $this->telegram_token . "/getWebhookInfo";
+        if ($this->debug) $this->logs[] = "Telegram URL: ".$url;
+        return json_decode($this->net->getContent($url, 5), true);
+    }
+
+    /**
      * Отправка сообщения в Телеграм Бот
      * @param string $message - сообщение
      * @param string $file - отправляемый файл
-     * @param int $token - ключ к Теллеграм
+     * @param integer $replay_to_id - ID сообщения на которое отвечаем
      * @param int $chat_id - идентификатор чата (человека, которому посылаем сообщение)
      * @return bool|false|string
      */
-    public function sendToTelegram ($message = '', $file = '', $token = 0, $chat_id = 0) {
+    public function sendToTelegram ($message = '', $file = '', $replay_to_id = 0, $chat_id = 0) {
         if ($this->debug) $this->logs[] = "Function sendToTelegram started";
-        if (!$token) $token = $this->telegram_token;
         if (!$chat_id) $chat_id = $this->telegram_id;
-        if ($this->debug) $this->logs[] = "Telegram token: ".$token;
         if ($this->debug) $this->logs[] = "Telegram Chat ID: ".$chat_id;
-        if (!$token || !$chat_id) {
+        if (!$this->telegram_token || !$chat_id) {
             $this->logs[] = "Wrong Telegram parameters!";
-            if (!$token) $this->logs[] = "Token not defined!";
+            if (!$this->telegram_token) $this->logs[] = "Token not defined!";
             else $this->logs[] = "Chat ID not defined!";
             return false;
         }
@@ -147,28 +241,35 @@ class Telegram {
 
         switch ($this->method) {
             case 'sendPhoto':
-                $data = array("chat_id"=>$chat_id, "photo" => $file_curl, "caption"=>$message);
+                if ($replay_to_id) $data = array("chat_id"=>$chat_id, "photo" => $file_curl, "reply_to_message_id" => $replay_to_id, "caption"=>$message);
+                else $data = array("chat_id"=>$chat_id, "photo" => $file_curl, "caption"=>$message);
                 break;
             case 'sendDocument':
-                $data = array("chat_id"=>$chat_id, "document" => $file_curl, "caption"=>$message);
+                if ($replay_to_id) $data = array("chat_id"=>$chat_id, "photo" => $file_curl, "reply_to_message_id" => $replay_to_id, "caption"=>$message);
+                else $data = array("chat_id"=>$chat_id, "document" => $file_curl, "caption"=>$message);
                 break;
             case 'sendVideo':
-                $data = array("chat_id"=>$chat_id, "video" => $file_curl, "caption"=>$message);
+                if ($replay_to_id) $data = array("chat_id"=>$chat_id, "photo" => $file_curl, "reply_to_message_id" => $replay_to_id, "caption"=>$message);
+                else $data = array("chat_id"=>$chat_id, "video" => $file_curl, "caption"=>$message);
                 break;
             case 'sendAudio':
-                $data = array("chat_id"=>$chat_id, "audio" => $file_curl, "caption"=>$message);
+                if ($replay_to_id) $data = array("chat_id"=>$chat_id, "photo" => $file_curl, "reply_to_message_id" => $replay_to_id, "caption"=>$message);
+                else $data = array("chat_id"=>$chat_id, "audio" => $file_curl, "caption"=>$message);
                 break;
             case 'sendVoice':
-                $data = array("chat_id"=>$chat_id, "voice" => $file_curl, "caption"=>$message);
+                if ($replay_to_id) $data = array("chat_id"=>$chat_id, "photo" => $file_curl, "reply_to_message_id" => $replay_to_id, "caption"=>$message);
+                else $data = array("chat_id"=>$chat_id, "voice" => $file_curl, "caption"=>$message);
                 break;
             case 'sendAnimation':
-                $data = array("chat_id"=>$chat_id, "animation" => $file_curl, "caption"=>$message);
+                if ($replay_to_id) $data = array("chat_id"=>$chat_id, "photo" => $file_curl, "reply_to_message_id" => $replay_to_id, "caption"=>$message);
+                else $data = array("chat_id"=>$chat_id, "animation" => $file_curl, "caption"=>$message);
                 break;
             default:
-                $data = array("chat_id"=>$chat_id, "text"=>$message);
+                if ($replay_to_id) $data = array("chat_id"=>$chat_id, "photo" => $file_curl, "reply_to_message_id" => $replay_to_id, "caption"=>$message);
+                else $data = array("chat_id"=>$chat_id, "text"=>$message);
         }
 
-        $url = "https://api.telegram.org/bot" . $token . "/".$this->method;
+        $url = "https://api.telegram.org/bot" . $this->telegram_token . "/".$this->method;
         if ($this->debug) $this->logs[] = "Telegram URL: ".$url;
 
         $t_bot = json_decode($this->net->getContent($url, 5, $data), true);
